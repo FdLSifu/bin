484090bb4d2a25929567754b17aee63f5697207b
FdLSifu <hamzin@gmail.com>
2021-06-12 14:00:05 +0200

    update

---
+++ scripts/output.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP1 --off --output DP1 --off --output DP1-1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --output DP1-2 --off --output DP1-3 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 9600x1080 --rotate normal --output VIRTUAL1 --off
